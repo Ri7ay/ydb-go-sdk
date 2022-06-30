5026f3a431205c263806014507e4cb748baa32f0
Timofey Koolin <timofey.koolin@gmail.com>
2022-06-30 12:56:43 +0300

    add comment

--- topic/client.go
+++ topic/client.go
@@ -10,7 +10,7 @@ type Client interface {
 	Close(context.Context) error
 
 	StartRead(
-		consumer string,
+		consumer string, // may be todo optional - for set in topic config, with optional redefine
 		readSelectors []topicreader.ReadSelector,
 		opts ...topicreader.ReaderOption,
 	) (*topicreader.Reader, error)
